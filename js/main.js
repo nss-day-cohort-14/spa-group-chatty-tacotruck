e22ec506e587a18d6f068d2a2f0225010d55de14
Vaughn Walters <vaughnwalters@gmail.com>
2016-06-14 10:09:55 -0500

    added event listeners to clear button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var darkThemeButton = document.getElementById("darkTheme");
 var largeTextButton = document.getElementById("largeText");
+var clearMessagesButton = document.getElementById("clearMessagesButton");
 var bodyColor = document.getElementById("body");
 var bodyTextSize = document.getElementById("body");
 
@@ -13,5 +14,12 @@ function toggleTextSize() {
   body.classList.toggle("textSizeChange");
 };
 
-largeText.addEventListener("click", toggleTextSize);
-darkTheme.addEventListener("click", toggleBackground);
+function clearMessages() {
+  console.log("clear Messages");
+  outputDiv.innerHTML = "";
+};
+
+largeTextButton.addEventListener("click", toggleTextSize);
+darkThemeButton.addEventListener("click", toggleBackground);
+clearMessagesButton.addEventListener("click", clearMessages);
+
