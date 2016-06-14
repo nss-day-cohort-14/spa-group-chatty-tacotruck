3be4f0b37c3c26c8365945caa503d8db929a1062
CJT1105 <CJT1105@GMAIL.COM>
2016-06-14 15:41:13 -0500

    fixed add message functionality

--- js/addMessage.js
+++ js/addMessage.js
@@ -11,43 +11,40 @@ var chatty = (function(originalFunction) {
 				if (cleared===true){
 					counter = 0;
 					counter ++;
-          inputObject.name = input.value
-					console.log("inputObject",inputObject );
-					messagesArray.push(inputObject);
-					console.log("messagesArray",messagesArray );
+					messagesArray.push(inputText.value);
 					input.value = "";
+          outputDiv.innerHTML = ""
   				for (let i =0; i < messagesArray.length; i++){
-  					outputDiv.innerHTML +=
+            outputDiv.innerHTML +=
       	 `
-      	<div id = "message--${counter}">${messagesArray[i].name}
+      	<div id = "message--${counter}">${messagesArray[i]}
         <button id="deleteButton--${counter}">delete</button>
-      	</div>
-    		`
-    		return inputObject
-  				}
+        </div>`
+      }
   				
 		} else {
-					messagesArray = chatty.getMessages();
-					populateOnEnter();
+          inputObject.name = input.value;
+          messagesArray.push(inputObject);
+          input.value = ''
+          for (let i =0; i < messagesArray.length; i++){
+            outputDiv.innerHTML +=
+         `
+        <div id = "message--${counter}">${messagesArray[i].name}
+        <button id="deleteButton--${counter}">delete</button>
+        </div>
+        `
+        return inputObject
+          }
+  
 				}
 			}
     })
   clearMessagesButton.addEventListener('click', function(){
   	 cleared = true;
+     messagesArray = []
   	 outputDiv.innerHTML = "";
   	 return cleared;
   	})
   return originalFunction;
 
 }(chatty || {}))
-
-function populateOnEnter(){
-	counter = 0;
-	inputObject.name = input.value;
-	console.log("inputObject",inputObject );
-  messagesArray.push(inputObject);
-  console.log("messagesArray", messagesArray );
-  input.value = "";
-  outputDiv.innerHTML = "";
-  populateMessagesDiv(messagesArray);
-}
